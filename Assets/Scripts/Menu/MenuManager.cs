9831335313df47ea2cdc061b0740ad6ddd8c7dbd
DanielRestrepo53 <84605991+Dread9182@users.noreply.github.com>
2022-08-10 18:34:45 -0500

    Paneles semifuncionales

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -6,8 +6,13 @@ using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
     //Cuando se presiona el boton Quit
-    public void  QuitButton() {
+    public void  ButtonQuit() {
         Debug.Log("sale del juego");
         Application.Quit();
+    }
+
+    //Cuando se presiona un boton que lleva a una nueva escena
+    public void ButtonEscena(string escena) {
+        SceneManager.LoadScene(escena);
     }
 }
