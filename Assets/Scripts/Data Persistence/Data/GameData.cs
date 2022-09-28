ccece2e1b21e0a0323e6d30956652ae33feeccfc
pablomesa08 <pablitomesa08@gmail.com>
2022-09-28 07:39:12 -0500

    SerializableDictionary

--- Assets/Scripts/Data Persistence/Data/GameData.cs
+++ Assets/Scripts/Data Persistence/Data/GameData.cs
@@ -9,13 +9,13 @@ public class GameData
     public float currentMaxHealth ;
     public float currentHealth;
     public Vector2 playerPosition;
-    public Dictionary<string,bool> healthCoins;
+    public SerializableDictionary<string,bool> healthCoins;
 
     public GameData(){
         maxHealth = 100f;
         currentMaxHealth = 100f;
         currentHealth = 100f;
         playerPosition = Vector2.zero;
-        healthCoins = new Dictionary<string, bool>();
+        healthCoins = new SerializableDictionary<string, bool>();
     }
 }
